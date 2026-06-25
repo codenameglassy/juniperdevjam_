f1135c9e5bd12522e9da122937a850869c76f801
codenameglassy <codenameglassy@gmail.com>
2026-06-25 20:40:46 +0545

    Level Decor

--- My project/Assets/LevelDecor.cs
+++ My project/Assets/LevelDecor.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +8,6 @@ public class LevelDecor : MonoBehaviour
     public List<GameObject> level3_decorToDisable = new List<GameObject>();
     public List<GameObject> level4_decorToDisable = new List<GameObject>();
 
-    // Start is called before the first frame update
     void Start()
     {
         ShowLevelSpecificDecor();
@@ -19,33 +17,19 @@ public class LevelDecor : MonoBehaviour
     {
         switch (LevelManager.Instance.CurrentLevel)
         {
-            case 1:
-                for (int i = 0; i < level1_decorToDisable.Count;)
-                {
-                    level1_decorToDisable[i].SetActive(false);
-                }
-                break;
+            case 1: DisableAll(level1_decorToDisable); break;
+            case 2: DisableAll(level2_decorToDisable); break;
+            case 3: DisableAll(level3_decorToDisable); break;
+            case 4: DisableAll(level4_decorToDisable); break;
+        }
+    }
 
-            case 2:
-                for (int i = 0; i < level2_decorToDisable.Count;)
-                {
-                    level2_decorToDisable[i].SetActive(false);
-                }
-                break;
-
-            case 3:
-                for (int i = 0; i < level3_decorToDisable.Count;)
-                {
-                    level3_decorToDisable[i].SetActive(false);
-                }
-                break;
-
-            case 4:
-                for (int i = 0; i < level4_decorToDisable.Count;)
-                {
-                    level4_decorToDisable[i].SetActive(false);
-                }
-                break;
+    void DisableAll(List<GameObject> decor)
+    {
+        foreach (GameObject go in decor)
+        {
+            if (go != null)
+                go.SetActive(false);
         }
     }
 }
